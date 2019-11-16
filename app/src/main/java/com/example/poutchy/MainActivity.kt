e7ccb44ec6db46f504c5bf8278505a0c65f45058
SihemBouhenniche <fs_bouhenniche@esi.dz>
2019-11-16 10:02:52 +0100

    first window creation

---
+++ app/src/main/java/com/example/poutchy/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.poutchy
+
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val bottomNavigation = findViewById<MeowBottomNavigation>(R.id.bottomNavigation)
+        bottomNavigation.add(MeowBottomNavigation.Model(1, R.drawable.ic_home))
+        bottomNavigation.add(MeowBottomNavigation.Model(2, R.drawable.ic_explore))
+        bottomNavigation.add(MeowBottomNavigation.Model(3, R.drawable.ic_message))
+    }
+}
